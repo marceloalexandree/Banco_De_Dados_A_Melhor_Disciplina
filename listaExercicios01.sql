c2d3c968cab47f4604ed58cdb6823ea4d8a8faaa
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-08-24 00:13:12 -0300

    Exercicio 04

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,7 @@
-SELECT titulo FROM livros;
+SELECT titulo FROM livros; /*Exercício 1*/
 
-SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+SELECT nome FROM autores WHERE nascimento < '1900-01-01'; /*Exercício 2*/
 
-SELECT livros.titulo, autores.nome FROM livros INNER JOIN autores ON livros.autor_id = autores.id AND autores.nome = 'J.K. Rowling';
+SELECT livros.titulo FROM livros INNER JOIN autores ON livros.autor_id = autores.id AND autores.nome = 'J.K. Rowling'; /*Exercício 3*/
+
+SELECT alunos.nome FROM alunos INNER JOIN matriculas ON alunos.id = matriculas.aluno_id AND matriculas.curso = 'Engenharia de Software'; /*Exercício 4*/
