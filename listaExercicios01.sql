4a15fd77263feee8efaec957dc08097a15d2a93d
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-08-24 00:24:20 -0300

    Exercicios05

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,3 +5,5 @@ SELECT nome FROM autores WHERE nascimento < '1900-01-01'; /*Exercício 2*/
 SELECT livros.titulo FROM livros INNER JOIN autores ON livros.autor_id = autores.id AND autores.nome = 'J.K. Rowling'; /*Exercício 3*/
 
 SELECT alunos.nome FROM alunos INNER JOIN matriculas ON alunos.id = matriculas.aluno_id AND matriculas.curso = 'Engenharia de Software'; /*Exercício 4*/
+
+SELECT produto, SUM(receita) as soma_receita FROM vendas GROUP BY produto; /*Exercício 5*/
