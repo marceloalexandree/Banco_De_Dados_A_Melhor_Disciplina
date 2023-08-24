39e1e2047c88e4a7822f77c39ae2cf9435e29991
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-08-23 23:55:41 -0300

    Exercicio 03

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,3 +1,5 @@
 SELECT titulo FROM livros;
 
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
+
+SELECT livros.titulo, autores.nome FROM livros INNER JOIN autores ON livros.autor_id = autores.id AND autores.nome = 'J.K. Rowling';
