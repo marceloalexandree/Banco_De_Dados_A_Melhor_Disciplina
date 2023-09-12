da8925113cfd74ec63aea6d611235e94a83d7889
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-09-11 23:47:24 -0300

    Exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -5,3 +5,12 @@ BEGIN
 END;
 // DELIMITER ;
 CALL sp_ListarAutores(); /*Exercício 1*/
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
+BEGIN
+	SELECT Titulo FROM Livro INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID AND Categoria.Nome = Nome_Categoria;
+END;
+// DELIMITER ;
+CALL sp_LivrosPorCategoria('Romance'); /*Exercício 2*/
+
