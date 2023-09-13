0ef2caedb1858ebc87e79ee5f3715d40047a71d0
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-09-13 18:03:06 -0300

    Exercício 4

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -6,6 +6,7 @@ END;
 // DELIMITER ;
 CALL sp_ListarAutores(); /*Exercício 1*/
 
+
 DELIMITER //
 CREATE PROCEDURE sp_LivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
 BEGIN
@@ -14,6 +15,7 @@ END;
 // DELIMITER ;
 CALL sp_LivrosPorCategoria('Romance'); /*Exercício 2*/
 
+
 DELIMITER //
 CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
 BEGIN
@@ -21,3 +23,21 @@ BEGIN
 END;
 // DELIMITER ;
 CALL sp_ContarLivrosPorCategoria('Autoajuda'); /*Exercício 3*/
+
+
+DELIMITER //
+DROP PROCEDURE sp_VerificarLivrosCategoria;
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN Nome_Categoria VARCHAR(100), OUT Possui_Livros VARCHAR(25))
+BEGIN
+	DECLARE qtd_livros INT;
+	SELECT COUNT(*) INTO qtd_livros FROM Livro INNER JOIN Categoria ON Categoria.Categoria_ID = Livro.Categoria_ID WHERE Categoria.Nome = Nome_Categoria GROUP BY Nome;
+    SET qtd_livros = qtd_livros;
+    IF qtd_livros > 0 THEN
+		SET Possui_Livros = 'Possui Livros';
+	ELSE
+		SET Possui_Livros = 'Não Possui Livros';
+	END IF;
+END;
+// DELIMITER ;
+CALL sp_VerificarLivrosCategoria('Autoajuda', @Possui_Livros);
+SELECT @Possui_Livros; /*Exercício 4*/
