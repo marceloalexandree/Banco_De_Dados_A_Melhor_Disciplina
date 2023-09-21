5115d5241cbf9a642b427b8d54dae13f5c90ed75
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-09-20 22:44:33 -0300

    Exercício 7

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,4 +1,4 @@
-DELIMITER //
+DELIMITER // /*Exercício 1*/
 CREATE PROCEDURE sp_ListarAutores()
 BEGIN
 	SELECT Nome FROM Autor;
@@ -7,7 +7,7 @@ END;
 CALL sp_ListarAutores(); /*Exercício 1*/
 
 
-DELIMITER //
+DELIMITER // /*Exercício 2*/
 CREATE PROCEDURE sp_LivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
 BEGIN
 	SELECT Titulo FROM Livro INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID AND Categoria.Nome = Nome_Categoria;
@@ -16,7 +16,7 @@ END;
 CALL sp_LivrosPorCategoria('Romance'); /*Exercício 2*/
 
 
-DELIMITER //
+DELIMITER // /*Exercício 3*/
 CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN Nome_Categoria VARCHAR(100))
 BEGIN
 	SELECT Categoria.Nome, COUNT(*) AS qtd_livros FROM Livro INNER JOIN Categoria ON Categoria.Categoria_ID = Livro.Categoria_ID WHERE Categoria.Nome = Nome_Categoria GROUP BY Nome;
@@ -25,7 +25,7 @@ END;
 CALL sp_ContarLivrosPorCategoria('Autoajuda'); /*Exercício 3*/
 
 
-DELIMITER //
+DELIMITER // /*Exercício 4*/
 DROP PROCEDURE sp_VerificarLivrosCategoria;
 CREATE PROCEDURE sp_VerificarLivrosCategoria(IN Nome_Categoria VARCHAR(100), OUT Possui_Livros VARCHAR(25))
 BEGIN
@@ -43,7 +43,7 @@ CALL sp_VerificarLivrosCategoria('Autoajuda', @Possui_Livros);
 SELECT @Possui_Livros; /*Exercício 4*/
 
 
-DELIMITER //
+DELIMITER // /*Exercício 5*/
 CREATE PROCEDURE sp_LivrosAteAno(ano INT)
 BEGIN
 	SELECT Titulo FROM Livro WHERE Ano_Publicacao = ano;
@@ -52,10 +52,26 @@ END;
 CALL sp_LivrosAteAno('2005'); /*Exercício 5*/
 
 
-DELIMITER //
+DELIMITER // /*Exercício 6*/
 CREATE PROCEDURE sp_TitulosPorCategoria(IN Nome_Categoria VARCHAR(100))
 BEGIN
 	SELECT Livro.Titulo FROM Livro INNER JOIN Categoria WHERE Categoria.Categoria_ID = Livro.Categoria_ID AND Categoria.Nome = Nome_Categoria;
 END;
 // DELIMITER ;
 CALL sp_TitulosPorCategoria('Romance'); /*Exercício 6*/
+
+
+DELIMITER // /*Exercício 7*/
+DROP PROCEDURE sp_AdicionarLivro;
+CREATE PROCEDURE sp_AdicionarLivro(IN Nome_Livro VARCHAR(100))
+BEGIN
+	DECLARE qtd_livros INT;
+    SELECT COUNT(Titulo) INTO qtd_livros FROM Livro WHERE Nome_Livro = Titulo;
+	IF qtd_livros = 0 THEN
+		INSERT INTO Livro (Titulo) VALUES (Nome_Livro);
+	ELSE
+		SELECT Titulo FROM Livro WHERE Titulo = Nome_Livro;
+	END IF;
+END;
+// DELIMITER ;
+CALL sp_AdicionarLivro('Marcelo Gostoso'); /*Exercício 7*/
