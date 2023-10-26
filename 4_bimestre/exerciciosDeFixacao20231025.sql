7d5e5201a7b3962b90bfc6b40ac5bd60150dae9b
marceloalexandree <116584202+marceloalexandree@users.noreply.github.com>
2023-10-25 21:37:40 -0300

    Exercício 2

--- 4_bimestre/exerciciosDeFixacao20231025.sql
+++ 4_bimestre/exerciciosDeFixacao20231025.sql
@@ -1,5 +1,11 @@
 /*Exercício 1*/
 CREATE TRIGGER before_clientes_insert 
-    BEFORE INSERT ON Clientes
-    FOR EACH ROW 
-	INSERT INTO Auditoria (mensagem, data_hora) VALUES ('Clientes atualizados', now());
+    	BEFORE INSERT ON Clientes
+	FOR EACH ROW 
+INSERT INTO Auditoria (mensagem, data_hora) VALUES ('Clientes atualizados', now());
+
+/*Exercício 2*/
+CREATE TRIGGER after_clientes_delete
+	AFTER DELETE ON Clientes
+	FOR EACH ROW
+INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão');
